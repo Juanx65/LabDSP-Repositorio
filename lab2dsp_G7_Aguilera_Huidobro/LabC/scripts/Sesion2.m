212e0a9b344a589d01ba9b6665c693272e99577e
cristobalhuidobro <cristobal.huidobro@sansano.usm.cl>
2021-05-18 17:12:47 -0400

    dddddddddddddddddd

--- lab2dsp_G7_Aguilera_Huidobro/LabC/scripts/Sesion2.m
+++ lab2dsp_G7_Aguilera_Huidobro/LabC/scripts/Sesion2.m
@@ -4,10 +4,13 @@ x=rand(10,1);
 %y = interpola(data,7);
 y = interpola(x,7);
 gd = 21;
-tx = 1:7:length(y);
-stem(tx,x);
+tx = 1:1/7:length(x);
+plot(tx(1:50),[y(gd:70)*(x(1)/(y(gd)))],'o');
 hold on
-plot([y(gd:70)*max(x)/max(y)],'o');
+stem(x);
+legend('Señal y luego de interpolar x con P = 7 y filtrar','Señal x aleatoria')
+xlabel('Muestras de x')
+ylabel('Amplitud')
 %% Parte I Aliasing Test
 
 load('aliasing_test_16_16.mat','aliasing_test');
@@ -18,29 +21,50 @@ t1=linspace(-Fs/2,Fs/2,length(aliasing_test));
 t2=linspace(-3*Fs/2,3*Fs/2,length(s_interpolada));
 subplot 311
 plot(t1,abs(fftshift(fft(aliasing_test))))
+xlabel('Frecuencia Hz')
+ylabel('Amplitud')
+title('Señal original')
 subplot 312
 plot(t2,abs(fftshift(fft(s_sin_filtro))))
+xlabel('Frecuencia Hz')
+ylabel('Amplitud')
+title('Señal interpolada sin filtrar')
 subplot 313
 plot(t2,abs(fftshift(fft(s_interpolada))))
+xlabel('Frecuencia Hz')
+ylabel('Amplitud')
+title('Señal interpolada filtrada')
 
 %Plots sin modificar fft
-%subplot 311
-%plot(abs(fft(aliasing_test)))
-%subplot 312
-%plot(abs(fft(s_sin_filtro)))
-%subplot 313
-%plot(abs(fft(s_interpolada)))
+subplot 311
+plot(abs(fft(aliasing_test)))
+ylabel('Amplitud')
+title('Señal original')
+subplot 312
+plot(abs(fft(s_sin_filtro)))
+ylabel('Amplitud')
+title('Señal interpolada sin filtrar')
+subplot 313
+plot(abs(fft(s_interpolada)))
+ylabel('Amplitud')
+title('Señal interpolada filtrada')
 
 
 %% Parte I Delta Kroeneker
 x=[1; zeros(39,1)];
 y = interpola(x,7);
 gd = 21;
-tx = 1:7:length(y);
+tx = 1:1/7:length(x);
 subplot 211
-stem(tx,x);
+stem(x);
+ylabel('Amplitud')
+xlabel('Muestras de x')
+title('Delta Krónecker')
 subplot 212
-plot([y*max(x)/max(y)],'o');
+plot(tx,[y(1:length(tx))*max(x)/max(y)],'o');
+ylabel('Amplitud')
+xlabel('Eqivalente en el tiempo a las muestras de x')
+title('Respuesta a delta Krónecker')
 
 %% Parte II Decimar
 load('aliasing_test_16_16.mat','aliasing_test');
@@ -50,8 +74,12 @@ t1=linspace(-Fs/2,Fs/2,length(aliasing_test));
 t2=linspace(-Fs/8,Fs/8,length(y));
 subplot 211
 plot(t1,abs(fftshift(fft(aliasing_test))))
+title('FFT de la señal aliasing test')
+xlabel('Frecuenciz Hz')
 subplot 212
 plot(t2,abs(fftshift(fft(y))))
+title('FFT de la señal aliasing test decimada')
+xlabel('Frecuenciz Hz')
 
 %% Parte III Remuestreo
 load('aliasing_test_16_16.mat','aliasing_test');
@@ -62,8 +90,13 @@ t1=linspace(-Fs/2,Fs/2,length(aliasing_test));
 t2=linspace(-3*Fs/8,3*Fs/8,length(y));
 subplot 211
 plot(t1,abs(fftshift(fft(aliasing_test))))
+title('FFT de la señal aliasing test')
+xlabel('Frecuenciz Hz')
 subplot 212
 plot(t2,abs(fftshift(fft(y))))
+title('FFT de la señal aliasing test remuestreada a 12kHz')
+xlabel('Frecuenciz Hz')
+audiowrite('aliasing_test_16_12.wav',y,Fs*3/4);
 %% Funciones
 
 function y=interpola(x,P)
