a979c80874a58f053451d68331da790ecf47d823
Juan Aguilera Castillo <59270808+Juanx65@users.noreply.github.com>
2021-05-31 02:59:55 -0400

    lab3_P1 II 2)
    algo intente hasta la parte II 2)

--- lab3dsp_G7_Aguilera_Huidobro/LabP1/scripts/Parte2.m
+++ lab3dsp_G7_Aguilera_Huidobro/LabP1/scripts/Parte2.m
@@ -8,20 +8,50 @@ w = 2*pi/3;
 
 subplot 211
 plot(w1,abs(y1));
+title('magnitud para N = 21')
+xlabel('frecuencia rad/muestra');
+ylabel('Amplitud')
 subplot 212
 plot(w1,angle(y1));
+title('fase para N = 21')
+xlabel('frecuencia rad/muestra');
+ylabel('fase rad');
 
 figure
 subplot 211
 plot(w2,abs(y2));
+title('Magnitud para N = 101');
+xlabel('frecuencia rad/muestra');
+ylabel('Amplitud')
 subplot 212
 plot(w2,angle(y2));
+title('fase para N = 101')
+xlabel('frecuencia rad/muestra');
+ylabel('fase rad');
 
 figure
 subplot 211
 plot(w3,abs(y3) );
+title('Magnitud para N = 1001');
+xlabel('frecuencia rad/muestra');
+ylabel('Amplitud')
 subplot 212
 plot(w3,angle(y3));
+title('fase para N = 1001')
+xlabel('frecuencia rad/muestra');
+ylabel('fase rad');
+%% 2)
+N = 100;
+subplot 511
+wvtool(rectwin(N))
+subplot 512
+wvtool(hann(N))
+subplot 513
+wvtool(hamming(N))
+subplot 514
+wvtool(blackman(N))
+subplot 515
+wvtool(bartlett(N))
 
 %% funcions
 function y = ventana1(w,N)
