af3de966e30e9823b24a15d933c5920009f3c471
Juan Aguilera Castillo <59270808+Juanx65@users.noreply.github.com>
2021-06-01 01:08:00 -0400

    lab3 P1 III 2)
    se intento

--- lab3dsp_G7_Aguilera_Huidobro/LabP1/scripts/Parte3.m
+++ lab3dsp_G7_Aguilera_Huidobro/LabP1/scripts/Parte3.m
@@ -23,6 +23,21 @@ freqz(B2)
 title('Ventana Blackman');
 
 %% 2)
+N1 = 70;
+N2 = 150;
+
+f = [0 0.1 0.15 0.35 0.4 0.65 0.75 1];
+m = [1 1 0 0 0.5 0.5 0 0];
+
+B1 = fir2(N1,f,m);
+B2 = fir2(N2,f,m);
+
+figure  
+freqz(B1)
+title('N = 70');
+figure
+freqz(B2)
+title('N = 150');
 
 
 %% 3)
