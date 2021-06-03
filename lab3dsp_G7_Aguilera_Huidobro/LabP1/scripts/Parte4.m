05f1dbf1b5bb68ccc9d827821587fc6b134f3571
Juan Aguilera Castillo <59270808+Juanx65@users.noreply.github.com>
2021-06-03 19:36:17 -0400

    lab3 IV 3)

--- lab3dsp_G7_Aguilera_Huidobro/LabP1/scripts/Parte4.m
+++ lab3dsp_G7_Aguilera_Huidobro/LabP1/scripts/Parte4.m
@@ -3,7 +3,7 @@ tm = 16000; %sps -> 2
 %a) 
 fc = 2000; %Hz
 Wp = fc*2/tm ;
-N = 10;
+N = 12;
 Rp = 0.5;
 Rs = 20;
 [B,A] = ellip(N,Rp,Rs,Wp,'low');
@@ -12,7 +12,7 @@ title('Filtro pasabajos fc = 2k Hz');
 %%  b)
 fc = 4000; %Hz
 Wp = fc*2/tm ;
-N = 10;
+N = 12;
 Rp = 0.5;
 Rs = 20;
 [B,A] = ellip(N,Rp,Rs,Wp,'high');
@@ -24,7 +24,7 @@ title('Filtro pasa-altos fc = 4k Hz');
 f1 = 2000; %Hz
 f2 = 4000;
 Wp = [f1*2/tm f2*2/tm];
-N = 10;
+N = 6;
 Rp = 0.5;
 Rs = 20;
 [B,A] = ellip(N,Rp,Rs,Wp);
@@ -36,7 +36,7 @@ title('Filtro pasa-banda f = [2k,4k] Hz');
 f1 = 2000; %Hz
 f2 = 4000;
 Wp = [f1*2/tm f2*2/tm];
-N = 10;
+N = 6;
 Rp = 0.5;
 Rs = 20;
 [B,A] = ellip(N,Rp,Rs,Wp,'stop');
@@ -47,7 +47,7 @@ title('Filtro elimina-banda f = [2k,4k] Hz');
 f1 = 2000; %Hz
 f2 = 4000;
 Wp = [f1*2/tm f2*2/tm];
-N = 4;
+N = 2;
 R = 2;
 [B,A] = cheby1(N,R,Wp);
 freqz(B,A);
@@ -56,9 +56,27 @@ title('Filtro Cheby1 pasa-banda f = [2k,4k] Hz');
 f1 = 2000; %Hz
 f2 = 4000;
 Wp = [f1*2/tm f2*2/tm];
-N = 4;
+N = 2;
 R = 20;
 [B,A] = cheby2(N,R,Wp);
 freqz(B,A);
 title('Filtro Cheby2 pasa-banda f = [2k,4k] Hz');
+
+%% 3)  1
+
+f1 = 800; %Hz
+f2 = 1600;
+Wp = [f1*2/tm f2*2/tm];
+N = 2;
+[B,A] = butter(N,Wp);
+freqz(B,A);
+title('Filtro Butterworth pasa-banda f = [800,1600] Hz');
+
+%% 3) 2
+
+[z,p,k] = butter(4,Wp);
+
+zplane(z,p,k);
+title('Plano Z Filtro Butterworth orden 8');
+
 
