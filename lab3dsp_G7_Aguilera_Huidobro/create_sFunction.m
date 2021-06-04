bf921b9375da21b2b77d950c158fb69ba2123254
cristobalhuidobro <cristobal.huidobro@sansano.usm.cl>
2021-06-03 22:18:36 -0400

    commit

--- lab3dsp_G7_Aguilera_Huidobro/create_sFunction.m
+++ lab3dsp_G7_Aguilera_Huidobro/create_sFunction.m
@@ -1,16 +1,16 @@
 % InstanciaciÃ³n del objeto
 def = legacy_code('initialize');
 % Nombre de la s-function
-def.SFunctionName = 'template_siso';
+def.SFunctionName = 'filtro_notch';
 % Protiotipo de la funciÃ³n
 % Notar es informaciÃ³n para Matlab, y el solo acepta nombres de entrada u1,
 % u2, etc, y salida y1
 % Usar el nombre de la funciÃ³n en C que implementÃ³
-def.OutputFcnSpec = 'double y1 = notch(double u1)';
+def.OutputFcnSpec = 'double y1 = retornar_salida(double u1, double u2)';
 % Archivo de cabecera
-def.HeaderFiles = {'notch.h'};
+def.HeaderFiles = {'filterInterface.h'};
 % Archivo fuente en C
-def.SourceFiles = {'notch.c'}; %cambiar entre 'lineal.c' 'lineal_int.c' o 'circular.c' en caso que sea necesario
+def.SourceFiles = {'filterInterface.c'};
 % CompilaciÃ³n para la s-function
 legacy_code('sfcn_cmex_generate', def)
 legacy_code('compile', def)
